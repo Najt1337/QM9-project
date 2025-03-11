3f3b520f589ba982e30483cadedb0d3b9d459388
Najt1337 <108531398+Najt1337@users.noreply.github.com>
2025-03-11 14:19:25 +0100

    further data preprocessing

--- QM9-project/qm9_preprocessing.py
+++ QM9-project/qm9_preprocessing.py
@@ -2,6 +2,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler,MinMaxScaler
+from tokenizer import SMILESTokenizer
+from itertools import repeat
 #%%
 data=np.load('qm9_data.npz',allow_pickle=True)
 data_smiles=pd.read_csv('QM9_SMILES.csv')
@@ -32,4 +34,22 @@ scaler2 = MinMaxScaler(feature_range=(-1, 1))
 for column in num_features.columns:
     scaler2.fit(num_features[column].values.reshape(-1, 1))
     num_features[column] = scaler2.transform(num_features[column].values.reshape(-1, 1))
+# %%
+tokenizer=SMILESTokenizer()
+SMILES_tokens_id=[]
+for SMILES in df['SMILES']:
+    ids=tokenizer.encode(SMILES)
+    SMILES_tokens_id.append(ids)
+
+# %%
+max_length=len(max(SMILES_tokens_id,key=len))
+print('SMILES max length:',max_length)
+# %%
+vocab_size=len(tokenizer.vocabulary)
+padding_id=tokenizer.vocabulary.index('<PAD>')
+print('Padding token id:',padding_id,'\n','Vocabulary size:',vocab_size)
+# %%
+SMILES_padded=SMILES_tokens_id
+for SMILES in SMILES_padded:
+    SMILES.extend(repeat(padding_id,max_length-len(SMILES)))
 # %%
